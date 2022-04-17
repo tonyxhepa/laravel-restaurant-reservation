b02af7a64424a70ff7afbb764927cb1bbf114a0e
jvict <jvictor.express2@gmail.com>
2022-04-17 00:33:09 -0300

    Fix: Attempt to read property menus on null

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,30 +113,32 @@
             </div>
         </div>
     </section>
-    <section class="mt-8 bg-white">
-        <div class="mt-4 text-center">
-            <h3 class="text-2xl font-bold">Our Menu</h3>
-            <h2 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
-                TODAY'S SPECIALITY</h2>
-        </div>
-        <div class="container w-full px-5 py-6 mx-auto">
-            <div class="grid lg:grid-cols-4 gap-y-6">
-                @foreach ($specials->menus as $menu)
-                    <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
-                        <img class="w-full h-48" src="{{ Storage::url($menu->image) }}" alt="Image" />
-                        <div class="px-6 py-4">
-                            <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">
-                                {{ $menu->name }}</h4>
-                            <p class="leading-normal text-gray-700">{{ $menu->description }}.</p>
-                        </div>
-                        <div class="flex items-center justify-between p-4">
-                            <span class="text-xl text-green-600">${{ $menu->price }}</span>
-                        </div>
-                    </div>
-                @endforeach
+    @if(is_array($specials))
+        <section class="mt-8 bg-white">
+            <div class="mt-4 text-center">
+                <h3 class="text-2xl font-bold">Our Menu</h3>
+                <h2 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+                    TODAY'S SPECIALITY</h2>
             </div>
-        </div>
-    </section>
+            <div class="container w-full px-5 py-6 mx-auto">
+                <div class="grid lg:grid-cols-4 gap-y-6">
+                    @foreach ($specials->menus as $menu)
+                        <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
+                            <img class="w-full h-48" src="{{ Storage::url($menu->image) }}" alt="Image" />
+                            <div class="px-6 py-4">
+                                <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">
+                                    {{ $menu->name }}</h4>
+                                <p class="leading-normal text-gray-700">{{ $menu->description }}.</p>
+                            </div>
+                            <div class="flex items-center justify-between p-4">
+                                <span class="text-xl text-green-600">${{ $menu->price }}</span>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+    @endif        
     <section class="pt-4 pb-12 bg-gray-800">
         <div class="my-16 text-center">
             <h2 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
